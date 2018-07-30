36c95f7d6a457280b3e18422b682c00597e9e613
Julieta Puente <julieta.puente@mercadolibre.com>
2018-07-30 11:41:54 -0300

    Caso de uso de ganar puntos

--- dojo_truco/javascript/src/test/solution.spec.js
+++ dojo_truco/javascript/src/test/solution.spec.js
@@ -59,6 +59,32 @@ describe("MyClass", () => {
             player2.play(new SieteDeBasto(), bajada);
         });
     });
+
+    it("El jugador 1 juega el ancho de espada y El jugador 2 juega el 4 de basto," +
+               "el 1 juega el tres de espada, el 2 el el ancho de basto, y por ultimo" +
+               "el jugador 2 juega el 7 de basto y el 1 juega el 4 de copas" +
+               "el jugador 1 no tiene puntos y el jugador 2 tiene 1 punto", () => {
+        var player1= new Player();
+        var player2 = new Player();
+
+        var bajada = new Bajada();
+
+        player1.pickup(new UnoDeEspada(), new TresDeEspada(), new CuatroDeCopa());
+        player2.pickup(new UnoDeBasto(), new CuatroDeBasto(), new SieteDeBasto());
+
+        bajada = player1.play(new UnoDeEspada());
+        bajada = player2.play(new CuatroDeBasto(), bajada);
+
+        bajada = player1.play(new TresDeEspada(), bajada);
+        bajada = player2.play (new UnoDeBasto(), bajada);
+
+        bajada = player1.play(new SieteDeBasto(), bajada);
+        bajada = player2.play(new CuatroDeCopa(), bajada);
+
+        chai.assert.equal(bajada.getPuntos().equal(new Punto(player2, new NadieCanto()), true);
+        //chai.assert.equal(bajada.getPuntosPlayer2().equal(new Punto(new NadieCanto()), true);
+        //chai.assert.equal(player2.getPuntos(), new Punto(new NadieCanto());
+    });
 });
 
 
